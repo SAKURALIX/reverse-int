30a5b36ea7bfcdd37e6deeab58f31d0d63730867
SAKURALIX <sakura.xlix@gmail.com>
2023-02-10 20:26:56 +0300

    feat: initial version not work properly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
+    n = String(n);
+    let stringToArray = n.split('');
   
-}
+    let reverseArray = stringToArray.reverse();
+  
+    let arrayToString = reverseArray.join();
+
+    let stringToNumber = Number(arrayToString);
+
+    let absoluteNumber = Math.abs(stringToNumber);
+  
+    return absoluteNumber
+  }
